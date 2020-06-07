01444e1724b3563d0c022d2b44f7f152c565f21d
Ross <yeahross@protonmail.com>
2020-06-07 18:40:24 +0100

    Move main loop to higher level

--- weegames/src/main.rs
+++ weegames/src/main.rs
@@ -297,6 +297,235 @@ impl<T> MessageBoxError for Result<T, String> {
     }
 }
 
+fn replace_text(object: &mut SerialiseObject, progress: &Progress) {
+    object.replace_text(progress.score, progress.lives);
+}
+
+fn run_main_loop<'a, 'b>(
+    mut game_mode: GameMode<'a, 'b>,
+    renderer: &mut Renderer,
+    event_pump: &mut EventPump,
+    imgui: &mut Imgui,
+    intro_font: &'a IntroFont<'a, 'b>,
+    config: &Config,
+) -> WeeResult<GameMode<'a, 'b>> {
+    match game_mode {
+        GameMode::Menu => {
+            let mut game = {
+                let filename = "games/system/main-menu.json";
+
+                LoadedGame::load(filename, &intro_font)
+                    .map_err(|error| format!("Couldn't load {}\n{}", filename, error))?
+                    .start(DEFAULT_GAME_SPEED, config.settings())
+            };
+
+            'menu_running: loop {
+                sdlglue::set_fullscreen(renderer, event_pump)?;
+
+                game.update_and_render_frame(renderer, event_pump)?;
+
+                if wee::is_switched_on(&game.objects, "Play") {
+                    game_mode = GameMode::Prelude;
+                    break 'menu_running;
+                }
+                if wee::is_switched_on(&game.objects, "Quit") {
+                    process::exit(0);
+                }
+            }
+        }
+        GameMode::Prelude => {
+            let (tx, rx) = mpsc::channel();
+
+            let handle = thread::spawn(move || -> WeeResult<()> {
+                let games_list = GamesList::new()?;
+
+                let filename = games_list.choose();
+
+                let game_data = GameData::load(&filename)?;
+
+                tx.send((games_list, game_data, filename))?;
+
+                Ok(())
+            });
+            match handle.join().unwrap() {
+                Ok(()) => {
+                    let completion = LoadedGame::load("games/system/prelude.json", &intro_font)?
+                        .start(DEFAULT_GAME_SPEED, config.settings())
+                        .play(renderer, event_pump)?;
+
+                    match rx.recv() {
+                        Ok((list, game_data, filename)) => {
+                            let games_list = list;
+
+                            let game =
+                                LoadedGame::load_from_game_data(game_data, &filename, &intro_font)?;
+
+                            let progress = Progress::new(config.playback_rate.min);
+
+                            log::info!("{:?}", games_list);
+
+                            game_mode = if let Completion::Finished = completion {
+                                GameMode::Play {
+                                    game,
+                                    games_list,
+                                    progress,
+                                }
+                            } else {
+                                GameMode::Menu
+                            }
+                        }
+                        Err(error) => {
+                            game_mode = GameMode::Error(Box::new(error));
+                        }
+                    }
+                }
+                Err(error) => {
+                    game_mode = GameMode::Error(error);
+                }
+            }
+        }
+        GameMode::Interlude {
+            won,
+            games_list,
+            progress,
+        } => {
+            let (tx, rx) = mpsc::channel();
+
+            let filename = games_list.choose();
+            thread::spawn(move || -> WeeResult<()> {
+                let new_game = {
+                    let game_data = GameData::load(&filename.clone())?;
+                    (filename, game_data)
+                };
+
+                tx.send(new_game)?;
+
+                Ok(())
+            });
+
+            let mut loaded_game = LoadedGame::load("games/system/interlude.json", &intro_font)?;
+
+            for object in loaded_game.objects.iter_mut() {
+                let mut set_switch = |name, pred| {
+                    if object.name == name {
+                        object.switch = if pred { Switch::On } else { Switch::Off };
+                    }
+                };
+                set_switch("Won", won);
+                set_switch("1", progress.lives >= 1);
+                set_switch("2", progress.lives >= 2);
+                set_switch("3", progress.lives >= 3);
+                set_switch("4", progress.lives >= 4);
+
+                replace_text(object, &progress);
+            }
+
+            let completion = loaded_game
+                .start(progress.playback_rate, config.settings())
+                .play(renderer, event_pump)?;
+
+            let (filename, game_data) = rx.recv()?;
+
+            let game = LoadedGame::load_from_game_data(game_data, &filename, &intro_font)?;
+
+            log::info!("Playing game: {}", filename);
+            game_mode = if let Completion::Finished = completion {
+                GameMode::Play {
+                    game,
+                    games_list,
+                    progress,
+                }
+            } else {
+                GameMode::Menu
+            };
+        }
+        GameMode::GameOver { progress } => {
+            let mut loaded_game = LoadedGame::load("games/system/game-over.json", &intro_font)?;
+            for object in loaded_game.objects.iter_mut() {
+                replace_text(object, &progress);
+            }
+            loaded_game
+                .start(DEFAULT_GAME_SPEED, config.settings())
+                .play(renderer, event_pump)?;
+
+            game_mode = GameMode::Menu;
+        }
+        GameMode::Play {
+            game: loaded_game,
+            games_list,
+            mut progress,
+        } => {
+            let mut game = loaded_game.start(progress.playback_rate, config.settings());
+
+            let result = game.play(renderer, event_pump);
+
+            match result {
+                Ok(completion) => {
+                    game_mode = if let Completion::Finished = completion {
+                        let has_won = game.has_been_won();
+
+                        progress.update(has_won, &config.playback_rate);
+                        log::info!("Playback Rate: {}", progress.playback_rate);
+
+                        if progress.lives == 0 {
+                            GameMode::GameOver { progress }
+                        } else {
+                            GameMode::Interlude {
+                                won: has_won,
+                                games_list,
+                                progress,
+                            }
+                        }
+                    } else {
+                        GameMode::Menu
+                    }
+                }
+                Err(error) => {
+                    log::error!("{}", error);
+                    game_mode = GameMode::Error(error);
+                }
+            }
+        }
+        //GameMode::Edit => {}
+        GameMode::Error(error) => {
+            let mut last_frame = Instant::now();
+            let mut do_break = false;
+            'error_running: loop {
+                if sdlglue::has_quit(event_pump) {
+                    process::exit(0);
+                }
+                sdlglue::set_fullscreen(renderer, event_pump)?;
+                sdlglue::clear_screen(Colour::dull_grey());
+
+                let (imgui_frame, ui) =
+                    imgui.prepare_frame(&renderer, &event_pump, &mut last_frame);
+
+                imgui::Window::new(im_str!("Error")).build(&ui, || {
+                    ui.text(format!("{}", error));
+                    if ui.button(im_str!("OK"), [100.0, 50.0]) {
+                        do_break = true;
+                    }
+                    ui.same_line(0.0);
+                    if ui.button(im_str!("Quit"), [100.0, 50.0]) {
+                        process::exit(0);
+                    }
+                });
+
+                if do_break {
+                    break 'error_running;
+                }
+
+                imgui_frame.render(&renderer, ui);
+
+                renderer.present();
+            }
+
+            game_mode = GameMode::Menu;
+        }
+    }
+    Ok(game_mode)
+}
+
 fn main() -> WeeResult<()> {
     init_logger();
 
@@ -348,241 +577,6 @@ fn main() -> WeeResult<()> {
     let mut game_mode = GameMode::Menu;
 
     loop {
-        fn replace_text(object: &mut SerialiseObject, progress: &Progress) {
-            object.replace_text(progress.score, progress.lives);
-        }
-
-        fn run_main_loop<'a, 'b>(
-            mut game_mode: GameMode<'a, 'b>,
-            renderer: &mut Renderer,
-            event_pump: &mut EventPump,
-            imgui: &mut Imgui,
-            intro_font: &'a IntroFont<'a, 'b>,
-            config: &Config,
-        ) -> WeeResult<GameMode<'a, 'b>> {
-            match game_mode {
-                GameMode::Menu => {
-                    let mut game = {
-                        let filename = "games/system/main-menu.json";
-
-                        LoadedGame::load(filename, &intro_font)
-                            .map_err(|error| format!("Couldn't load {}\n{}", filename, error))?
-                            .start(DEFAULT_GAME_SPEED, config.settings())
-                    };
-
-                    'menu_running: loop {
-                        sdlglue::set_fullscreen(renderer, event_pump)?;
-
-                        game.update_and_render_frame(renderer, event_pump)?;
-
-                        if wee::is_switched_on(&game.objects, "Play") {
-                            game_mode = GameMode::Prelude;
-                            break 'menu_running;
-                        }
-                        if wee::is_switched_on(&game.objects, "Quit") {
-                            process::exit(0);
-                        }
-                    }
-                }
-                GameMode::Prelude => {
-                    let (tx, rx) = mpsc::channel();
-
-                    let handle = thread::spawn(move || -> WeeResult<()> {
-                        let games_list = GamesList::new()?;
-
-                        let filename = games_list.choose();
-
-                        let game_data = GameData::load(&filename)?;
-
-                        tx.send((games_list, game_data, filename))?;
-
-                        Ok(())
-                    });
-                    match handle.join().unwrap() {
-                        Ok(()) => {
-                            let completion =
-                                LoadedGame::load("games/system/prelude.json", &intro_font)?
-                                    .start(DEFAULT_GAME_SPEED, config.settings())
-                                    .play(renderer, event_pump)?;
-
-                            match rx.recv() {
-                                Ok((list, game_data, filename)) => {
-                                    let games_list = list;
-
-                                    let game = LoadedGame::load_from_game_data(
-                                        game_data,
-                                        &filename,
-                                        &intro_font,
-                                    )?;
-
-                                    let progress = Progress::new(config.playback_rate.min);
-
-                                    log::info!("{:?}", games_list);
-
-                                    game_mode = if let Completion::Finished = completion {
-                                        GameMode::Play {
-                                            game,
-                                            games_list,
-                                            progress,
-                                        }
-                                    } else {
-                                        GameMode::Menu
-                                    }
-                                }
-                                Err(error) => {
-                                    game_mode = GameMode::Error(Box::new(error));
-                                }
-                            }
-                        }
-                        Err(error) => {
-                            game_mode = GameMode::Error(error);
-                        }
-                    }
-                }
-                GameMode::Interlude {
-                    won,
-                    games_list,
-                    progress,
-                } => {
-                    let (tx, rx) = mpsc::channel();
-
-                    let filename = games_list.choose();
-                    thread::spawn(move || -> WeeResult<()> {
-                        let new_game = {
-                            let game_data = GameData::load(&filename.clone())?;
-                            (filename, game_data)
-                        };
-
-                        tx.send(new_game)?;
-
-                        Ok(())
-                    });
-
-                    let mut loaded_game =
-                        LoadedGame::load("games/system/interlude.json", &intro_font)?;
-
-                    for object in loaded_game.objects.iter_mut() {
-                        let mut set_switch = |name, pred| {
-                            if object.name == name {
-                                object.switch = if pred { Switch::On } else { Switch::Off };
-                            }
-                        };
-                        set_switch("Won", won);
-                        set_switch("1", progress.lives >= 1);
-                        set_switch("2", progress.lives >= 2);
-                        set_switch("3", progress.lives >= 3);
-                        set_switch("4", progress.lives >= 4);
-
-                        replace_text(object, &progress);
-                    }
-
-                    let completion = loaded_game
-                        .start(progress.playback_rate, config.settings())
-                        .play(renderer, event_pump)?;
-
-                    let (filename, game_data) = rx.recv()?;
-
-                    let game = LoadedGame::load_from_game_data(game_data, &filename, &intro_font)?;
-
-                    log::info!("Playing game: {}", filename);
-                    game_mode = if let Completion::Finished = completion {
-                        GameMode::Play {
-                            game,
-                            games_list,
-                            progress,
-                        }
-                    } else {
-                        GameMode::Menu
-                    };
-                }
-                GameMode::GameOver { progress } => {
-                    let mut loaded_game =
-                        LoadedGame::load("games/system/game-over.json", &intro_font)?;
-                    for object in loaded_game.objects.iter_mut() {
-                        replace_text(object, &progress);
-                    }
-                    loaded_game
-                        .start(DEFAULT_GAME_SPEED, config.settings())
-                        .play(renderer, event_pump)?;
-
-                    game_mode = GameMode::Menu;
-                }
-                GameMode::Play {
-                    game: loaded_game,
-                    games_list,
-                    mut progress,
-                } => {
-                    let mut game = loaded_game.start(progress.playback_rate, config.settings());
-
-                    let result = game.play(renderer, event_pump);
-
-                    match result {
-                        Ok(completion) => {
-                            game_mode = if let Completion::Finished = completion {
-                                let has_won = game.has_been_won();
-
-                                progress.update(has_won, &config.playback_rate);
-                                log::info!("Playback Rate: {}", progress.playback_rate);
-
-                                if progress.lives == 0 {
-                                    GameMode::GameOver { progress }
-                                } else {
-                                    GameMode::Interlude {
-                                        won: has_won,
-                                        games_list,
-                                        progress,
-                                    }
-                                }
-                            } else {
-                                GameMode::Menu
-                            }
-                        }
-                        Err(error) => {
-                            log::error!("{}", error);
-                            game_mode = GameMode::Error(error);
-                        }
-                    }
-                }
-                //GameMode::Edit => {}
-                GameMode::Error(error) => {
-                    let mut last_frame = Instant::now();
-                    let mut do_break = false;
-                    'error_running: loop {
-                        if sdlglue::has_quit(event_pump) {
-                            process::exit(0);
-                        }
-                        sdlglue::set_fullscreen(renderer, event_pump)?;
-                        sdlglue::clear_screen(Colour::dull_grey());
-
-                        let (imgui_frame, ui) =
-                            imgui.prepare_frame(&renderer, &event_pump, &mut last_frame);
-
-                        imgui::Window::new(im_str!("Error")).build(&ui, || {
-                            ui.text(format!("{}", error));
-                            if ui.button(im_str!("OK"), [100.0, 50.0]) {
-                                do_break = true;
-                            }
-                            ui.same_line(0.0);
-                            if ui.button(im_str!("Quit"), [100.0, 50.0]) {
-                                process::exit(0);
-                            }
-                        });
-
-                        if do_break {
-                            break 'error_running;
-                        }
-
-                        imgui_frame.render(&renderer, ui);
-
-                        renderer.present();
-                    }
-
-                    game_mode = GameMode::Menu;
-                }
-            }
-            Ok(game_mode)
-        }
-
         let loop_result = run_main_loop(
             game_mode,
             &mut renderer,
